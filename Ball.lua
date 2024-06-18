fad3ab77b81cb1a4f6263a6477817aa46d71e72f
Young <zcy19990107@gmail.com>
2024-06-18 16:39:24 +0800

    final version

---
+++ Ball.lua
@@ -0,0 +1,42 @@
+Ball = Class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- keeping track of velocity 
+    self.dy = 0
+    self.dx = 0
+end
+
+-- returns true or false
+function Ball:collides(paddle)
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+        return false
+    end 
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dx = 0
+    self.dy = 0
+end
+
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
---
+++ Paddle.lua
@@ -0,0 +1,26 @@
+Paddle = Class{}
+
+--[[
+    The `init` function on our class is called just once, when the object
+    is first created. Used to set up all variables in the class and get it
+    ready for use.
+]]
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
+
+function Paddle:update(dt)
+    if self.dy < 0 then
+        self.y = math.max(0, self.y + self.dy * dt)
+    else
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
